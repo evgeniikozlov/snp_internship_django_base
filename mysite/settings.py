eb31bcb3c6bed308fd483330e14f3a7cad4d1b44
Evgenii Kozlov <kozlov.evg.a@gmail.com>
2022-11-24 21:10:31 +0300

    moved secret to separate file added local settings added gitignore

--- mysite/settings.py
+++ mysite/settings.py
@@ -18,11 +18,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-bv%i0b=sr^ig4h!(#ra9&6y4f%7^^&!(^lur$e$f=ge=j01)t@'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+try:
+    from mysite.secret import SECRET_KEY
+except ImportError:
+    raise ImportError("SECRET_KEY is not specified in secret.py file")
 
 ALLOWED_HOSTS = []
 
@@ -116,3 +116,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
